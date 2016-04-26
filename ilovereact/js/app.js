11de2aecffae81fb8c0c9a2ed61377090a29355c
baurine <2008.hbl@gmail.com>
2016-04-26 18:09:08 +0800

    feat: use GPU accelerate for animation, and animate android robot

--- ilovereact/js/app.js
+++ ilovereact/js/app.js
@@ -1,25 +1,34 @@
-function animatLogo() {
-	TweenMax.fromTo("#react-logo", 3, {
-		css: {
-			top: "-50px",
-		}
-	}, {
-		css: {
-			top: "20px",
-		},
+function animateLogo() {
+    TweenMax.fromTo("#react-logo", 3, {
+        css: {
+            // use "y" replace "top", can accelerate by GPU
+            y: "-50px",
+        }
+    }, {
+        css: {
+            y: "20px",
+        },
 
-		repeat: -1,
+        repeat: -1,
 
-		yoyo: true,
+        yoyo: true,
 
-		ease: Power2.easeInOut,
-	});
+        ease: Power2.easeInOut,
+    });
 }
 
-/*
-window.onload = function() {
-	animatLogo();
-};
-*/
+function animateRobot() {
+    var t = new TimelineMax({repeat: -1});
+    t.to("#android-robot", 0.4, {rotation: "-=10deg"})
+        .to("#android-robot", 0.8, {rotation: "+=20deg"})
+        .to("#android-robot", 0.4, {rotation: "-=10deg"});
+}
 
+window.onload = function() {
+    animateLogo();
+    animateRobot();
+};
+
+/*
 window.onload = animatLogo;
+*/
