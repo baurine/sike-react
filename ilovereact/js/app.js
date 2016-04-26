4d171d238563581ac0a24c65e8a79409844a0237
baurine <2008.hbl@gmail.com>
2016-04-26 22:48:17 +0800

    feat: update scroll control when page scroll

--- ilovereact/js/app.js
+++ ilovereact/js/app.js
@@ -24,9 +24,32 @@ function animateRobot() {
         .to("#android-robot", 0.4, {rotation: "-=10deg"});
 }
 
+function updateSliderControl() {
+    var links = document.querySelectorAll("#slider-control a");
+
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+
+        var section = document.querySelector(link.getAttribute("href"));
+        var sectionTop = section.offsetTop;
+        var sectionBottom = sectionTop + window.innerHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+            link.className = "active";
+        } else {
+            link.className = "";
+        }
+    }
+}
+
+window.onscroll = function() {
+    updateSliderControl();
+}
+
 window.onload = function() {
     animateLogo();
     animateRobot();
+    updateSliderControl();
 };
 
 /*
