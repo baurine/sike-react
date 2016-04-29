22f0252008eff03485c0220837b15528cf3fd004
baurine <2008.hbl@gmail.com>
2016-04-29 14:16:58 +0800

    feat: react the whole layout

--- buyshoes-react/build/app.js
+++ buyshoes-react/build/app.js
@@ -7,11 +7,113 @@ var App = React.createClass({
         return React.createElement(
             "div",
             { className: "site" },
+            React.createElement(Bg, null),
+            React.createElement(SiteMain, null),
+            React.createElement(SiteRightSiderBar, null),
+            React.createElement(SiteRightSiderBarToggle, null)
+        );
+    }
+});
+
+var Bg = React.createClass({
+    displayName: "Bg",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "bg" },
+            React.createElement("div", { className: "bg__img" })
+        );
+    }
+});
+
+var SiteMain = React.createClass({
+    displayName: "SiteMain",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "site__main" },
             React.createElement(
-                "h1",
-                null,
-                "Buy Shoes!"
+                "div",
+                { className: "site__left-sidebar" },
+                React.createElement(SiteTitle, null)
+            ),
+            React.createElement(
+                "div",
+                { className: "site__content" },
+                React.createElement(Products, null)
             )
+        );
+    }
+});
+
+var SiteRightSiderBar = React.createClass({
+    displayName: "SiteRightSiderBar",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "site__right-sidebar" },
+            React.createElement(Cart, null),
+            React.createElement(Checkout, null)
+        );
+    }
+});
+
+var SiteRightSiderBarToggle = React.createClass({
+    displayName: "SiteRightSiderBarToggle",
+
+    render: function render() {
+        return React.createElement(
+            "a",
+            { className: "site__right-sidebar-toggle" },
+            React.createElement("img", { src: "img/arrow-icon.svg" })
+        );
+    }
+});
+
+var SiteTitle = React.createClass({
+    displayName: "SiteTitle",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "title" },
+            React.createElement(
+                "h2",
+                null,
+                "Buy Me Shoes"
+            ),
+            React.createElement("img", { className: "title__heart", src: "img/heart.svg" })
+        );
+    }
+});
+
+var Products = React.createClass({
+    displayName: "Products",
+
+    render: function render() {
+        return React.createElement("div", { className: "products" });
+    }
+});
+
+var Cart = React.createClass({
+    displayName: "Cart",
+
+    render: function render() {
+        return React.createElement("div", { className: "cart" });
+    }
+});
+
+var Checkout = React.createClass({
+    displayName: "Checkout",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "checkout" },
+            "Checkout"
         );
     }
 });
--- buyshoes-react/js/app.jsx
+++ buyshoes-react/js/app.jsx
@@ -2,7 +2,96 @@ let App = React.createClass({
     render() {
         return (
             <div className="site">
-                <h1>Buy Shoes!</h1>
+              <Bg/>
+              <SiteMain/>
+              <SiteRightSiderBar/>
+              <SiteRightSiderBarToggle/>
+            </div>
+        );
+    }
+});
+
+let Bg = React.createClass({
+    render() {
+        return (
+            <div className="bg">
+                <div className="bg__img"></div>
+            </div>
+        );
+    }
+});
+
+let SiteMain = React.createClass({
+    render() {
+        return (
+            <div className="site__main">
+              <div className="site__left-sidebar">
+                <SiteTitle/>
+              </div>
+
+              <div className="site__content">
+                <Products/>
+              </div>
+            </div>
+        );
+    }
+});
+
+let SiteRightSiderBar = React.createClass({
+    render() {
+        return (
+            <div className="site__right-sidebar">
+              <Cart/>
+              <Checkout/>
+            </div>
+        );
+    }
+});
+
+let SiteRightSiderBarToggle = React.createClass({
+    render() {
+        return (
+            <a className="site__right-sidebar-toggle">
+              <img src="img/arrow-icon.svg" />
+            </a>
+        );
+    }
+});
+
+let SiteTitle = React.createClass({
+    render() {
+        return (
+            <div className="title">
+              <h2>Buy Me Shoes</h2>
+              <img className="title__heart" src="img/heart.svg"/>
+            </div>
+        );
+    }
+});
+
+let Products = React.createClass({
+    render() {
+        return (
+            <div className="products">
+            </div>
+        );
+    }
+});
+
+let Cart = React.createClass({
+    render() {
+        return (
+            <div className="cart">
+            </div>
+        );
+    }
+});
+
+let Checkout = React.createClass({
+    render() {
+        return (
+            <div className="checkout">
+              Checkout
             </div>
         );
     }
