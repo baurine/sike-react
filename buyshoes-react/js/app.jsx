c6e3d0954bb5398b932b1663f10845741ad45407
baurine <2008.hbl@gmail.com>
2016-04-30 18:22:55 +0800

    feat: use Webpack pack all js files

--- buyshoes-react/js/app.jsx
+++ buyshoes-react/js/app.jsx
@@ -1,3 +1,9 @@
+//const Ps = require("../node_modules/perfect-scrollbar/index")
+//const React = require("../node_modules/react/react")
+const Ps = require("perfect-scrollbar");
+const React = require("react");
+//let {pi, e} = require("./pie");
+
 let App = React.createClass({
     render() {
         return(
